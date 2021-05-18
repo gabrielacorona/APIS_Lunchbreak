cbe6e384f6dd38967d088b173ac83f6ee926f7c7
carooobregon <carooobregon@gmail.com>
2021-05-18 16:32:37 -0700

    added fonts

--- APIS_Lunchbreak/AppDelegate.swift
+++ APIS_Lunchbreak/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        for family in UIFont.familyNames.sorted() {
+            let names = UIFont.fontNames(forFamilyName: family)
+            print("Family: \(family) Font names: \(names)")
+        }
         FirebaseApp.configure()
         return true
     }
