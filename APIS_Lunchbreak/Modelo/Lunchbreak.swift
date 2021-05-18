87a36506430c5212e882066edcf8304d90efbb35
gabrielacorona <gabriela.corona03@gmail.com>
2021-05-18 13:45:01 -0500

    adding views and models for functionalities

---
+++ APIS_Lunchbreak/Modelo/Lunchbreak.swift
@@ -0,0 +1,20 @@
+//
+//  Lunchbreak.swift
+//  APIS_Lunchbreak
+//
+//  Created by Gaby Corona on 5/18/21.
+//
+
+import Foundation
+import UIKit
+
+class Lunchbreak:Codable{
+    var id:String = ""
+    var host:String = ""
+    var time: String = ""
+    var  whatToEat: String = ""
+    var location: String = ""
+    
+    var joinedBy: [String] = []
+}
+
--- APIS_Lunchbreak/Views/VCAddUserViewController.swift
+++ APIS_Lunchbreak/Views/VCAddUserViewController.swift
@@ -15,7 +15,17 @@ class VCAddUserViewController: UIViewController {
     @IBOutlet weak var pass: UITextField!
     @IBOutlet weak var confirmPass: UITextField!
     @IBOutlet weak var createUser: UIButton!
+    @IBOutlet weak var mexicanFood: UIButton!
+    @IBOutlet weak var koreanFood: UIButton!
+    @IBOutlet weak var italianFood: UIButton!
+    @IBOutlet weak var VegetarianFood: UIButton!
+    @IBOutlet weak var healthyFood: UIButton!
+    @IBOutlet weak var ketoFood: UIButton!
+    @IBOutlet weak var fastFood: UIButton!
+    @IBOutlet weak var veganFood: UIButton!
+    @IBOutlet weak var tfDescription: UITextField!
     
+    var favFoods:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Create User"
@@ -26,7 +36,7 @@ class VCAddUserViewController: UIViewController {
     @IBAction func addUser(_ sender: UIButton) {
     
         if pass.text == confirmPass.text {
-            DatabaseManager.shared.createUser(fName:fName.text!,lName:lName.text!, email:email.text!, password:pass.text!)
+            DatabaseManager.shared.createUser(fName:fName.text!,lName:lName.text!, email:email.text!, password:pass.text!, description:tfDescription.text!)
         }else{
             let alert = UIAlertController(title: "Passwords Don't Match", message: "Passwords Don't Match, Please Verify", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
